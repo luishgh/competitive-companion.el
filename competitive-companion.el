31e0c45b1d9ac3fe2e2279a6dfef654b1f6f492a
Luis Higino <luishenriquegh2701@gmail.com>
2024-12-25 19:56:57 +0000

    Rename file and minor improvements
    * Add typical fields to header
    * Reorganize the contests of the file
    * Remove more dead code

--- competitive-companion.el
+++ competitive-companion.el
@@ -1,4 +1,27 @@
-;;; competitive-companion.el --- Wrapper for Competitive Companion -*- lexical-binding: t; -*-
+;;; competitive-companion.el --- Competitive Companion Integration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Luis Higino
+
+;; Author: Luis Higino <luis.higino@dcc.ufmg.br>
+;; URL: https://github.com/luishgh/competitive-companion.el
+;; Created: 25 Dec 2024
+;; Package-Requires: ((emacs "28.1"))
+;; Keywords: convenience
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This plugin provides an Emacs interface to interact with the Competitive Companion browser extension.
@@ -7,14 +30,28 @@
 
 ;;; Code:
 
-(require 'json)
+;;;; Requirements
+
 (require 'cl-lib)
+(require 'json)
 (require 'server)
+
+;;;; Variables
+
+(defvar-local competitive-companion--current-task nil
+  "Holds the current task's (i.e. problem), associated with its data dir.")
+
+(defvar competitive-companion--contest-directory nil
+  "Holds the current contest's directory.")
+
+;;;; Customization
 
 (defgroup competitive-companion nil
   "Competitive Companion integration for Emacs."
   :group 'tools
   :prefix "competitive-companion-")
+
+;;;;; Options
 
 (defcustom competitive-companion-task-major-mode 'c++-mode
   "Major mode for task's implementation file."
@@ -28,6 +65,11 @@ the task file header."
   :type 'file
   :group 'competitive-companion)
 
+(defcustom competitive-companion-server-port 10050
+  "Port on which the Competitive Companion server listens."
+  :type 'integer
+  :group 'competitive-companion)
+
 (defun competitive-companion--default-task-extension ()
   "Get the default file extension based on `competitive-companion-task-major-mode'."
   (pcase competitive-companion-task-major-mode
@@ -39,12 +81,9 @@ the task file header."
     ('javascript-mode ".js")
     (_ ".txt")))
 
-(defvar-local competitive-companion--current-task nil
-  "Holds the current task's (i.e. problem), associated with its data dir.")
+;;;; Commands
 
-(defvar competitive-companion--contest-directory nil
-  "Holds the current contest's directory.")
-
+;;;###autoload
 (define-minor-mode competitive-companion-mode
   "Minor mode for Competitive Companion integration."
   :lighter " CC"
@@ -58,10 +97,39 @@ the task file header."
     (delete-process "*competitive-companion-server*")
     (message "Competitive Companion mode deactivated.")))
 
-(defcustom competitive-companion-server-port 10050
-  "Port on which the Competitive Companion server listens."
-  :type 'integer
-  :group 'competitive-companion)
+(defun competitive-companion-run-tests (command)
+  "Run all test cases for the current task using COMMAND.
+Reports success if all tests pass, or failure otherwise."
+  (interactive (list (read-shell-command "Command to run: " competitive-companion--contest-directory)))
+  (let* ((default-directory competitive-companion--current-task)
+         (test-files (directory-files default-directory t "^input[0-9]+\.txt$"))
+         (failed-outputs "")
+         (all-success t))
+    (dolist (input-file test-files)
+      (let* ((match-input (file-name-nondirectory input-file))
+             (_ (string-match "[0-9]+" match-input))
+             (index (match-string 0 match-input))
+             (output-file (format "output%s.txt" index))
+             (actual-output (shell-command-to-string (format "%s < %s" command input-file)))
+             (input-text (with-temp-buffer
+                                (insert-file-contents input-file)
+                                (buffer-string)))
+             (expected-output (with-temp-buffer
+                                (insert-file-contents output-file)
+                                (buffer-string))))
+        (unless (string= actual-output expected-output)
+          (setq all-success nil)
+          (setq failed-outputs
+                (concat failed-outputs
+                  (format "* Test %s failed.\n** Input:\n%s\n** Expected:\n%s\n** Got:\n%s\n" index input-text expected-output actual-output)))))
+    (if all-success
+        (message "All tests passed!")
+      (with-help-window "*competitive-companion-output*"
+        (outline-minor-mode)
+        (message "Some tests failed.")
+        (insert failed-outputs))))))
+
+;;;; Functions
 
 (defun competitive-companion--start-server ()
   "Start a Competitive Companion server to receive problem data."
@@ -126,44 +194,6 @@ the task file header."
                  (output-file (expand-file-name (format "output%d.txt" index) directory)))
              (write-region (alist-get 'input test) nil input-file)
              (write-region (alist-get 'output test) nil output-file))))
-
-(defun competitive-companion-start ()
-  "Start the Competitive Companion server."
-  (interactive)
-  (competitive-companion--start-server)
-  (message "Competitive Companion server started on port %d" competitive-companion-server-port))
-
-(defun competitive-companion-run-tests (command)
-  "Run all test cases for the current task using COMMAND.
-Reports success if all tests pass, or failure otherwise."
-  (interactive (list (read-shell-command "Command to run: " (concat competitive-companion--contest-directory "/"))))
-  (let* ((default-directory competitive-companion--current-task)
-         (test-files (directory-files default-directory t "^input[0-9]+\.txt$"))
-         (failed-outputs "")
-         (all-success t))
-    (dolist (input-file test-files)
-      (let* ((match-input (file-name-nondirectory input-file))
-             (match (string-match "[0-9]+" match-input))
-             (index (match-string 0 match-input))
-             (output-file (format "output%s.txt" index))
-             (actual-output (shell-command-to-string (format "%s < %s" command input-file)))
-             (input-text (with-temp-buffer
-                                (insert-file-contents input-file)
-                                (buffer-string)))
-             (expected-output (with-temp-buffer
-                                (insert-file-contents output-file)
-                                (buffer-string))))
-        (unless (string= actual-output expected-output)
-          (setq all-success nil)
-          (setq failed-outputs
-                (concat failed-outputs
-                  (format "* Test %s failed.\n** Input:\n%s\n** Expected:\n%s\n** Got:\n%s\n" index input-text expected-output actual-output)))))
-    (if all-success
-        (message "All tests passed!")
-      (with-help-window "*competitive-companion-output*"
-        (outline-minor-mode)
-        (message "Some tests failed.")
-        (insert failed-outputs))))))
 
 (provide 'competitive-companion)
 ;;; competitive-companion.el ends here
